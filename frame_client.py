4a055e2318b91361fc07b4785a08c49cd571187d
TinD <tindao2011@gmail.com>
2025-02-16 18:24:14 -0500

    Rename frame client to be importable

--- frame_client.py
+++ frame_client.py
@@ -26,10 +26,11 @@ def recv():
         return frame
     return None
 
-while True:
-    frame = recv()
-    if frame is None:
-        break
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+if __name__ == "__main__":
+    while True:
+        frame = recv()
+        if frame is None:
+            break
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
