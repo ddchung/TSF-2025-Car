b88aabb9d2ad66b1b6e3cb4cfba8022945168c39
TinD <tindao2011@gmail.com>
2025-02-17 13:25:24 -0500

    update steering line

--- steering_line.py
+++ steering_line.py
@@ -5,7 +5,7 @@ import math
 def draw_steering_curve(image, steering_angle):
     # angle from -90 to 90
 
-    CALIBRATION = 20
+    CALIBRATION = 40
 
     steering_angle = steering_angle / 90
     height, width = image.shape[:2]
@@ -16,15 +16,19 @@ def draw_steering_curve(image, steering_angle):
     for i in range(1, 100):
         x1 = points[i - 1][0]
         y1 = points[i - 1][1]
-        x2 = int(np.sin(angle * np.pi / 2) * (10) + x1)
-        y2 = int(y1 - np.cos(angle * np.pi / 2) * (10))
+        x2 = int(np.sin(angle / CALIBRATION * np.pi / 2) * (10) + x1)
+        y2 = int(y1 - np.cos(angle / CALIBRATION * np.pi / 2) * (10))
         if x1 < 0 or x1 > width or y1 < 0 or y1 > height:
             break
         points.append((x2, y2))
-        angle += steering_angle / CALIBRATION
+        angle += steering_angle
     
     for i in range(1, len(points)):
-        cv2.line(image, points[i - 1], points[i], (0, 255, 0), 2)
+        x1, y1 = points[i - 1]
+        x2, y2 = points[i]
+        y1 = y1 // 2 + height // 2
+        y2 = y2 // 2 + height // 2
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
     
     return image
 
