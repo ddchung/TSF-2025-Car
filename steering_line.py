8a21ea0da742732194c6ed40405db0a7f274e535
TinD <tindao2011@gmail.com>
2025-02-17 19:49:18 -0500

    Update steering line code

--- steering_line.py
+++ steering_line.py
@@ -2,34 +2,46 @@ import cv2
 import numpy as np
 import math
 
-def draw_steering_curve(image, steering_angle):
+def draw_steering_curve(image, steering_angle, length=None):
     # angle from -90 to 90
 
-    CALIBRATION = 40
+    # CALIBRATION = 15
 
-    steering_angle = steering_angle / 90
-    height, width = image.shape[:2]
+    # steering_angle = steering_angle / 90
+    # height, width = image.shape[:2]
 
-    points = []
-    angle = steering_angle
-    points.append((width // 2, height))
-    for i in range(1, 100):
-        x1 = points[i - 1][0]
-        y1 = points[i - 1][1]
-        x2 = int(np.sin(angle / CALIBRATION * np.pi / 2) * (10) + x1)
-        y2 = int(y1 - np.cos(angle / CALIBRATION * np.pi / 2) * (10))
-        if x1 < 0 or x1 > width or y1 < 0 or y1 > height:
-            break
-        points.append((x2, y2))
-        angle += steering_angle
+    # points = []
+    # angle = steering_angle * 2
+    # points.append((width // 2, height))
+    # for i in range(1, 100):
+    #     x1 = points[i - 1][0]
+    #     y1 = points[i - 1][1]
+    #     x2 = int(np.sin(angle / CALIBRATION * np.pi / 2) * (10) + x1)
+    #     y2 = int(y1 - np.cos(angle / CALIBRATION * np.pi / 2) * (10))
+    #     if x1 < 0 or x1 > width or y1 < 0 or y1 > height:
+    #         break
+    #     points.append((x2, y2))
+    #     angle += steering_angle
     
-    for i in range(1, len(points)):
-        x1, y1 = points[i - 1]
-        x2, y2 = points[i]
-        y1 = y1 // 2 + height // 2
-        y2 = y2 // 2 + height // 2
-        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
-    
+    # for i in range(1, len(points)):
+    #     x1, y1 = points[i - 1]
+    #     x2, y2 = points[i]
+    #     y1 = y1 // 2 + height // 2
+    #     y2 = y2 // 2 + height // 2
+    #     cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    CALIBRATION = 1.6
+
+    steering_angle *= CALIBRATION
+
+    x1 = image.shape[1] // 2
+    y1 = image.shape[0]
+    length = x1 if length is None else length
+    x2 = int(np.sin(steering_angle / 90 * np.pi / 2) * (length) + x1)
+    y2 = int(y1 - np.cos(steering_angle / 90 * np.pi / 2) * (length))
+
+    cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
     return image
 
 if __name__ == "__main__":
