75204615020b06341e810de768ba78fc7fa73764
TinD <tindao2011@gmail.com>
2025-03-02 19:37:37 -0500

    update test

--- test3.py
+++ test3.py
@@ -1,50 +1,15 @@
 import cv2
 import rc_car_api
-import sys
 
 import lane_follower
-
-import blue_detect
-
-for x in range(len(sys.argv)):
-    if sys.argv[x] == "-r":
-        x += 1
-        lane_follower.REG_OF_INT = float(sys.argv[x])
-    if sys.argv[x] == "-lg":
-        x += 1
-        lane_follower.LOWER_GREEN_HSV = int(sys.argv[x])
-    if sys.argv[x] == "-ug":
-        x += 1
-        lane_follower.UPPER_GREEN_HSV = int(sys.argv[x])
-    if sys.argv[x] == "-sf":
-        lane_follower.SHOW_FRAME = True
-    if sys.argv[x] == "-sh":
-        lane_follower.SHOW_HSV = True
-    if sys.argv[x] == "-sm":
-        lane_follower.SHOW_MASKED = True
-    if sys.argv[x] == "-se":
-        lane_follower.SHOW_CROPPED_EDGES = True
-    if sys.argv[x] == "-sl":
-        lane_follower.SHOW_LINES = True
-    if sys.argv[x] == "-sll":
-        lane_follower.SHOW_LANE_LINES = True
-    if sys.argv[x] == "-sq":
-        qr_detect.SHOW_QR_CODES = True
-
-# start webcam
-cap = cv2.VideoCapture(0)
-
-cap.set(3, 480)
-cap.set(4, 240)
+import frame_client
+import correct_fov
+import white_balance
 
 def calculate_move(frame):
     # Calculate lane lines and steering angle
     lane_lines = lane_follower.detect_lane(frame)
     steering_angle = lane_follower.compute_steering_angle(frame, lane_lines)
-    
-    new_angle = blue_detect.follow_blue(frame)
-    #if new_angle != None:
-    #    steering_angle = new_angle
 
     # Calculate whether or not we should move
     # Placeholder for now
@@ -62,30 +27,31 @@ def calculate_move(frame):
 
 steering_angle = 90
 
-rc_car_api.initCar()
-
 while True:
-    success, frame = cap.read()
-    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    
-    if not success:
+    frame = frame_client.recv()
+    if frame is None:
         break
+    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    frame = correct_fov.correct(frame)
+    frame = white_balance.automatic_white_balance(frame)
     if lane_follower.SHOW_FRAME:
         cv2.imshow("frame", frame)
 
     steering_angle, should_move = calculate_move(frame)
 
-    rc_car_api.setSteeringAngle(steering_angle - 90)
+    rc_car_api.set_steering_angle(steering_angle - 90)
     
     key = cv2.waitKey(1)
     if key == ord('w') and should_move:
-        rc_car_api.moveForward(1, time=1)
+        rc_car_api.start_move_forward(30)
     
     if key == ord('s') and should_move:
-        rc_car_api.moveBackward(1, time=1)
+        rc_car_api.start_move_backward(30)
+
+    if key == ord(' '):
+        rc_car_api.stop_move()
     
     if key == ord('q'):
         break
 
-cap.release()
 cv2.destroyAllWindows()
