c3b7b9ce8ced32499b30c573b4fba159a3814592
TinD <tindao2011@gmail.com>
2025-03-05 18:01:27 -0500

    next revision of main

--- main-threading-3.py
+++ main-threading-3.py
@@ -10,171 +10,458 @@ import threading
 import tensorflow as tf
 from dataclasses import dataclass
 from collections import defaultdict
+from typing import Any, Callable
+import sys
+import os
 
-CAM_WIDTH = 480
-CAM_HEIGHT = 360
-
-# Wrapper class to automatically lock/unlock
-class SyncedVariable:
-    def __init__(self, value):
-        self._value = value
-        self._lock = threading.Lock()
-
-    def get(self):
-        with self._lock:
-            return self._value
-
-    def set(self, value):
-        with self._lock:
-            self._value = value
-
-
-class PredictedObjects:
-    def __init__(self):
-        self._objects = defaultdict(set)
-        
-    def add_object(self, obj_class, x, y, w, h):
-        self._objects[obj_class].add((x, y, w, h))
-    
-    def get_objects(self, obj_class):
-        return self._objects[obj_class]
-    
-    def get_all_objects(self):
-        return self._objects
-    
-    def clear_objects(self, obj_class):
-        del self._objects[obj_class]
-    
-    def clear_all_objects(self):
-        self._objects.clear()
-
+# state structure
 @dataclass
-class CarState:
-    speed : int
-    steering_angle : int
-    headlights : int
-    taillights : int
-    sun : int
+class State:
+    frame: np.ndarray
+    distance_sensors: list[int]
+    objects: list[tuple[str, int, int, int, int]]
+    steering: int
 
-@dataclass
-class EnvironState:
-    predicted_objects : PredictedObjects
-    predicted_steering : int
-    frame : np.ndarray
+    speed: int
+    speed_limit: int
+    lights: list[int]
 
-# Global variables
-environ_state = SyncedVariable(EnvironState(PredictedObjects(), 0, np.zeros((CAM_HEIGHT, CAM_WIDTH, 3), dtype=np.uint8)))
-speed_limit = SyncedVariable(0.3)
 
-# actions
-# signature: (CarState) -> CarState, bool
-# returns: updated car state, whether to continue with other actions
-
-def check_person(car_state):
-    people = environ_state.get().predicted_objects.get_objects('person')
-
-    # in percent
-    ACTIVE_AREA = ((0., 1.), (1., 1.), (0.3, 0.5), (0.7, 0.5))
-    ACTIVE_AREA = ((int(x * CAM_WIDTH), int(y * CAM_HEIGHT)) for x, y in ACTIVE_AREA)
-    ACTIVE_AREA = tuple(ACTIVE_AREA)
-
-    for person in people:
-        # check if person is in active area
-        x, y, w, h = person
-        c_x, c_y = x + w // 2, y + h // 2
-        if ACTIVE_AREA[0][0] < c_x < ACTIVE_AREA[1][0] and ACTIVE_AREA[0][1] < c_y < ACTIVE_AREA[1][1]:
-            car_state.speed = 0
-            car_state.taillights = 1
-            return car_state, False
-    return car_state, True
-
-def check_stop_sign(car_state):
-    stop_signs = environ_state.get().predicted_objects.get_objects('stop')
-
-    if stop_signs:
-        # TODO: make start moving after 3 seconds
-        car_state.speed = 0
-        car_state.taillights = 1
-        return car_state, False
-    return car_state, True
-
-def check_red_light(car_state):
-    red_lights = environ_state.get().predicted_objects.get_objects('red light')
-
-    if red_lights:
-        car_state.speed = 0
-        car_state.taillights = 1
-        return car_state, False
-    return car_state, True
-
-def check_green_light(car_state):
-    green_lights = environ_state.get().predicted_objects.get_objects('green light')
-
-    if green_lights:
-        car_state.speed = speed_limit.get()
-        car_state.taillights = 0.5
-    return car_state, True
-
-def check_speed_limit(car_state):
-    MIN_SPEED_LIM = 10
-    MAX_SPEED_LIM = 120
-    STEP_SPEED_LIM = 10
-
-    for i in range(MIN_SPEED_LIM, MAX_SPEED_LIM + 1, STEP_SPEED_LIM):
-        speed_limit = environ_state.get().predicted_objects.get_objects(f'speed limit {i}')
-
-        if speed_limit:
-            car_state.speed = i / 3
-            break
-    return car_state, True
-
-def correct_brightness(car_state):
-    ADJ_TO = 80 # 0-255
-
-    frame = environ_state.get().frame
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    brightness = np.mean(frame[:,:,2])
-
-    correction = ADJ_TO - brightness
-    correction /= 4 # stabilize
-    car_state.headlights *= correction
-    car_state.sun *= correction
-    return car_state, True
-
-def correct_steering(car_state):
-    angle = environ_state.get().predicted_steering
-    car_state.steering_angle = angle
-    return car_state, True
-
-# priority: first to last
-actions = [
-    # should always correct, so put at top
-    correct_brightness,
-    correct_steering,
-
-    check_person,
-    check_stop_sign,
-    check_red_light,
-    check_green_light,
-    check_speed_limit,
+# global state
+state = [
+    State(
+        frame=np.zeros((480, 640, 3), dtype=np.uint8),
+        distance_sensors=[0, 0, 0, 0],
+        objects=[],
+        steering=0,
+        speed=0,
+        speed_limit=40,
+        lights=[0, 0, 0]
+    )
 ]
 
-def run_actions(car_state) -> CarState:
+state_lock = threading.Lock()
+
+def update_car():
+    # get the current state
+    with state_lock:
+        current_state = state[0]
+    
+    # update the car
+    rc_car_api.set_steering_angle(current_state.steering)
+    rc_car_api.start_move_forward(current_state.speed)
+    rc_car_api.set_light(0, current_state.lights[0])
+    rc_car_api.set_light(1, current_state.lights[1])
+    rc_car_api.set_light(3, current_state.lights[2])
+
+
+def do_actions(actions):
     for action in actions:
-        car_state, cont = action(car_state)
-        if not cont:
+        try:
+            action()
+        except StopIteration:
             break
-    return car_state
+
+
+# actions
+
+def example_action():
+    THRESHOLD = 0.1 # 10%
+
+    global state
+    global state_lock
+
+    # get objects
+    with state_lock:
+        objects = state[0].objects
+        cam_height, cam_width = state[0].frame.shape[:2]
+    
+    # search for some object bigger than some amount
+    found = False
+    for obj in objects:
+        name, x, y, w, h = obj
+        if w * h > THRESHOLD * cam_height * cam_width:
+            print(f'Found object {name} at {x}, {y}')
+            found = True
+            break
+    if not found:
+        print('No object found')
+        return
+    
+    # do something
+    with state_lock:
+        state[0].speed = 0
+        state[0].steering = 0
+        state[0].lights = [0, 0, 0]
+    
+    # can either return or raise StopIteration
+    # return continues with other actions
+    # raise StopIteration stops doing other actions
+    raise StopIteration
+
+def check_speed_limit():
+    # checks for any `speed limit %d`
+    THRESHOLD = 0.05 # 5%
+
+    global state
+    global state_lock
+
+    # get objects
+    with state_lock:
+        objects = state[0].objects
+        cam_height, cam_width = state[0].frame.shape[:2]
+    
+    # search for speed limit bigger than some amount
+    limit = None
+    for obj in objects:
+        name, x, y, w, h = obj
+        if (name.startswith('speed limit') 
+                and w * h > THRESHOLD * cam_height * cam_width):
+            try:
+                limit = int(name.split()[-1])
+            except ValueError:
+                print(f"WARNING: Invalid speed limit {name}")
+                return
+            break
+    if limit is None:
+        return
+    
+    # set speed limit
+    with state_lock:
+        state[0].speed_limit = limit
+
+def correct_speed():
+    global state
+    global state_lock
+
+    with state_lock:
+        cur_speed = state[0].speed
+        speed_limit = state[0].speed_limit
+        if cur_speed > 0:
+            state[0].speed = speed_limit
+        elif cur_speed < 0:
+            state[0].speed = -speed_limit
+        # don't do anything if speed is 0
+
+def check_distance_sensors():
+    STOP_THRESHOLD = 5 # 5 cm
+    BACK_THRESHOLD = 3 # 10 cm
+
+    global state
+    global state_lock
+
+    with state_lock:
+        front, back = state[0].distance_sensors[:2]
+    
+    if front < STOP_THRESHOLD:
+        with state_lock:
+            cur_speed = state[0].speed
+            if cur_speed > 0:
+                print("Front obstacle detected, stopping")
+                state[0].speed = 0
+            if front < BACK_THRESHOLD:
+                print("Front obstacle close, going back")
+                state[0].speed = -1
+        raise StopIteration
+    elif back < STOP_THRESHOLD:
+        with state_lock:
+            cur_speed = state[0].speed
+            if cur_speed < 0:
+                print("Back obstacle detected, stopping")
+                state[0].speed = 0
+            if back < BACK_THRESHOLD:
+                print("Back obstacle close, going forward")
+                state[0].speed = 1
+        raise StopIteration
+
+def check_red_light():
+    THRESHOLD = 0.05 # 5%
+
+    global state
+    global state_lock
+
+    # get objects
+    with state_lock:
+        objects = state[0].objects
+        cam_height, cam_width = state[0].frame.shape[:2]
+    
+    # search for red light bigger than some amount
+    found = False
+    for obj in objects:
+        name, x, y, w, h = obj
+        if (name == 'red light' 
+                and w * h > THRESHOLD * cam_height * cam_width):
+            found = True
+            break
+    if not found:
+        return
+    
+    # do something
+    with state_lock:
+        print("Red light detected, stopping")
+        state[0].speed = 0
+    raise StopIteration
+
+def check_person():
+    THRESHOLD = 0.1 # 10%
+    MAX_DISTANCE = 0.2 # max 20% radius, otherwise ignore
+
+    global state
+    global state_lock
+
+    # get objects
+    with state_lock:
+        objects = state[0].objects
+        cam_height, cam_width = state[0].frame.shape[:2]
+    
+    # search for person bigger than some amount
+    found = False
+    for obj in objects:
+        name, x, y, w, h = obj
+        # for person, additionally check if it's within the bottom-center of the frame
+        dx = x + w/2 - cam_width/2
+        dy = y + h - cam_height
+        d = np.sqrt(dx**2 + dy**2)
+        if (name == 'person' 
+                and w * h > THRESHOLD * cam_height * cam_width
+                and d < MAX_DISTANCE * cam_height):
+            found = True
+            break
+    if not found:
+        return
+    
+    # do something
+    with state_lock:
+        print("Person detected, stopping")
+        state[0].speed = 0
+    raise StopIteration    
+
+last_stop_sign_stop = -1
+last_stop_sign_go = 0
+def check_stop_sign():
+    THRESHOLD = 0.15 # 15%
+    STOP_TIME = 3 # 3 seconds
+    GET_OUT_TIME = 5 # 5 seconds
+
+    global state
+    global state_lock
+    global last_stop_sign_stop
+    global last_stop_sign_go
+
+    # get objects
+    with state_lock:
+        objects = state[0].objects
+        cam_height, cam_width = state[0].frame.shape[:2]
+    
+    # search for stop sign bigger than some amount
+    found = False
+    for obj in objects:
+        name, x, y, w, h = obj
+        if (name == 'stop' 
+                and w * h > THRESHOLD * cam_height * cam_width):
+            found = True
+            break
+    if not found:
+        return
+    
+    # stop for 3 seconds, then go
+    now = time.time()
+    if last_stop_sign_stop < last_stop_sign_go:
+        if now - last_stop_sign_go > GET_OUT_TIME:
+            # give car GET_OUT_TIME seconds to get out
+            print("Detected stop sign, waiting")
+            last_stop_sign_stop = now
+            with state_lock:
+                state[0].speed = 0
+            raise StopIteration
+    else:
+        # already stopped
+        if now - last_stop_sign_stop > STOP_TIME:
+            # stopped for STOP_TIME seconds
+            print("Done stopping at stop sign")
+            last_stop_sign_go = now
+            with state_lock:
+                state[0].speed = 1 # let correct_speed() handle the speed
+            return
+        else:
+            # still stopping
+            with state_lock:
+                state[0].speed = 0
+            raise StopIteration
+
+def check_green_light():
+    THRESHOLD = 0.05 # 5%
+
+    global state
+    global state_lock
+
+    # get objects
+    with state_lock:
+        objects = state[0].objects
+        cam_height, cam_width = state[0].frame.shape[:2]
+    
+    # search for green light bigger than some amount
+    found = False
+    for obj in objects:
+        name, x, y, w, h = obj
+        if (name == 'green light' 
+                and w * h > THRESHOLD * cam_height * cam_width):
+            found = True
+            break
+    if not found:
+        return
+    
+    # do something
+    with state_lock:
+        print("Green light detected, going")
+        state[0].speed = 1
+
+# note: top ones have higher priority
+actions = [
+    check_speed_limit,
+    correct_speed,
+    check_distance_sensors,
+    check_red_light,
+    check_person,
+    check_stop_sign,
+    check_green_light
+]
 
 # Input loops
 
-def poll_camera():
-    while True:
-        frame = frame_client.get_frame()
-        if frame is None:
-            continue
-        environ_state.set(environ_state.get()._replace(frame=frame))
-        
+# frame
+def frame_loop():
+    global state
+    global state_lock
 
-def main():
-    car_state = CarState(0, 0, 0, 0, 0)
+    cap = cv2.VideoCapture(0)
+    while True:
+        # frame = frame_client.recv()
+        ok, frame = cap.read()
+        # if frame is None:
+        #     log("WARN: No frame")
+        #     continue
+        if not ok:
+            print("WARN: no frame")
+            continue
+        with state_lock:
+            state[0].frame = frame
+        time.sleep(0.01)
+
+# distance sensors
+def distance_sensor_loop():
+    global state
+    global state_lock
+
+    while True:
+        sensors = [rc_car_api.read_sensor(i) for i in range(4)]
+        with state_lock:
+            state[0].distance_sensors = sensors
+        time.sleep(0.01)
+
+# objects
+traffic_sign_model = YOLO('traffic_sign_detector.pt')
+person_model = YOLO('yolov10n.pt')
+def object_loop():
+    global state
+    global state_lock
+
+    while True:
+        print("predicting")
+        with state_lock:
+            frame_ = state[0].frame
+        print("got frame, shape:", frame_.shape)
+        objects = traffic_sign_model.predict(frame_, conf=0.6, verbose = False)
+        people = person_model.predict(frame_, conf=0.2, classes=[0], verbose = False) # chose a much lower value since lego people don't look that similar to people
+        detected = []
+        for object in objects:
+            for box in object.boxes:
+                x, y, w, h = box.xywh[0]
+                x = int(x)
+                y = int(y)
+                w = int(w)
+                h = int(h)
+                x -= w // 2
+                y -= h // 2
+                name = object.names[int(box.cls)].lower()
+                detected.append((name, x, y, w, h))
+        for person in people:
+            for box in person.boxes:
+                x, y, w, h = box.xywh[0]
+                x = int(x)
+                y = int(y)
+                w = int(w)
+                h = int(h)
+                x -= w // 2
+                y -= h // 2
+                detected.append(('person', x, y, w, h))
+        
+        print("detected", detected)
+        print("writing")
+        with state_lock:
+            state[0].objects = detected
+        print("done")
+
+# steering
+steering_model = tf.keras.models.load_model('lane_nav_model_final.keras')
+def steering_loop():
+    global state
+    global state_lock
+
+    while True:
+        with state_lock:
+            image = state[0].frame
+        height, _ = image.shape[:2]
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_green = (50, 40, 40)
+        upper_green = (100, 255, 230)
+        mask = cv2.inRange(hsv, lower_green, upper_green)
+        white = 255 * np.ones_like(image)
+        white = cv2.bitwise_and(white, white, mask=mask)
+        white = cv2.cvtColor(white, cv2.COLOR_BGR2GRAY)
+        image = white[int(height/2):, :]
+        image = cv2.GaussianBlur(image, (3, 3), 0)
+        image = cv2.resize(image, (200, 66))
+        image = image / 255
+        image = np.asarray([image])
+        steering = steering_model.predict(image, verbose = 0)
+        with state_lock:
+            state[0].steering =  (steering - 90) * 1.5
+
+def main_loop():
+    global state
+    global state_lock
+
+    black = np.zeros((480, 640, 3), np.uint8)
+
+    traffic_sign_model.predict(black, conf=0.6, verbose = False)
+    person_model.predict(black, conf=0.2, classes=[0], verbose = False)
+
+    # start threads
+    threading.Thread(target=frame_loop, daemon=True).start()
+    threading.Thread(target=distance_sensor_loop, daemon=True).start()
+    threading.Thread(target=object_loop, daemon=True).start()
+    threading.Thread(target=steering_loop, daemon=True).start()
+
+    print("Started main loop")
+
+    while True:
+        with state_lock:
+            current_state = state[0]
+        do_actions(actions)
+        update_car()
+        print(current_state.objects)
+        annotated = current_state.frame.copy()
+        objects = current_state.objects
+
+        for obj in objects:
+            name, x, y, w, h = obj
+            cv2.rectangle(annotated, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(annotated, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('frame', annotated)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+if __name__ == '__main__':
+    main_loop()
