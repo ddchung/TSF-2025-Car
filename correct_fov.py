87d270ba13f7fe34b3a500b93950d0f037db3348
TinD <tindao2011@gmail.com>
2025-03-07 11:45:25 -0500

    update fov correction code

--- correct_fov.py
+++ correct_fov.py
@@ -19,12 +19,13 @@ def crop_frame(frame, top, bottom, left, right):
 def defish(frame):
     dtype = 'linear'
     format = 'fullframe'
-    fov = 170
-    new_fov = 120
+    fov = 175
+    new_fov = 137
     return defisheye.Defisheye(frame, dtype=dtype, format=format, fov=fov, pfov=new_fov).convert()
 
 def correct(frame):
     frame = crop_frame(frame, 0, 0.29, 0.15, 0)
+    cv2.imshow("cropped", frame)
     frame = defish(frame)
     return frame
 
@@ -32,13 +33,21 @@ if __name__ == "__main__":
     import white_balance
     video = cv2.VideoCapture('/home/tin/Downloads/Screen Recording 2025-02-25 at 8.04.05 AM.mov')
     while True:
-        for _ in range(10):
-            video.read()
         ok, frame = video.read()
         if not ok:
             break
         frame = correct(frame)
         frame = white_balance.automatic_white_balance(frame)
         cv2.imshow("frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(0)
+        if key == ord('q'):
             break
+        if key == ord('n'):
+            for _ in range (10):
+                video.read()
+        if key == ord('m'):
+            for _ in range (50):
+                video.read()
+        if key == ord(','):
+            for _ in range (100):
+                video.read()
